0752a4698183222f545245733f29c41b684c3144
theark00 <143406995+theark00@users.noreply.github.com>
2024-08-07 01:34:51 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,38 +9,44 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Logging.AddConsole();
 
+
 builder.Services.AddControllersWithViews();
-builder.Services.AddHttpClient();
+builder.Services.AddHttpClient("AuthService", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7034");
+});
 builder.Services.AddDbContext<ApplicationContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("WebApp_Database")));
 builder.Services.AddIdentity<UserEntity, IdentityRole>(x =>
 {
     x.SignIn.RequireConfirmedAccount = false;
     x.User.RequireUniqueEmail = true;
     x.Password.RequiredLength = 8;
-
 }).AddEntityFrameworkStores<ApplicationContext>();
+
 builder.Services.ConfigureApplicationCookie(x =>
 {
-    x.LoginPath = "/signin";
+    x.LoginPath = "/api/auth/signin";
     x.Cookie.HttpOnly = true;
     x.Cookie.SecurePolicy = CookieSecurePolicy.Always;
     x.ExpireTimeSpan = TimeSpan.FromHours(1);
     x.SlidingExpiration = true;
 });
 
-// Configure logging
+
 builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 
 var app = builder.Build();
+
+
 app.UseHsts();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Default}/{action=Home}/{id?}");
 
 app.Run();
-
